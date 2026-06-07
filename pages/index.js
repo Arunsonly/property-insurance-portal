1a6122a0377e9fa664f3bd98c88e6ac51801eebc
Arunsonly <arunwidad@gmail.com>
2026-06-07 16:45:24 +0530

    Create Home component for Property Insurance Portal
    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>Property Insurance Portal</h1>
+      <p>Portal setup in progress...</p>
+    </div>
+  );
+}
