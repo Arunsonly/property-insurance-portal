5e973d4cd8a14ae7129733a4a55a8a726ce4b260
Arunsonly <arunwidad@gmail.com>
2026-06-07 23:05:23 +0530

    Update dashboard.js

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,1 +1,15 @@
-
+export default function UserDashboard() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#f8fafc",
+        fontFamily: "Arial,sans-serif",
+        padding: "20px",
+      }}
+    >
+      <h1>User Dashboard</h1>
+      <p>Dashboard Working Successfully</p>
+    </div>
+  );
+}
